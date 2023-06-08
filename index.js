4c2bf1d1487fc7cce428dae3c3cb45c15277730e
MostofaAsik <mmostofaasik@gmail.com>
2023-06-08 20:59:18 +0600

    saved-user-mongo

--- index.js
+++ index.js
@@ -26,6 +26,27 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const usersCollection = client.db('sportsDb').collection('users')
+
+        //users created related api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user?.email }
+            const existingUser = await usersCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exist' })
+            }
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
