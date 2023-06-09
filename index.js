9d9cd117ac8df739670adeae874ef03e03a3e78a
MostofaAsik <mmostofaasik@gmail.com>
2023-06-09 22:23:07 +0600

    verifyJWt

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.use(express.json())
 app.use(cors())
 
 
+//verifyJWT
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unauthorized access" })
+    }
+    const token = authorization.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvjkksn.mongodb.net/?retryWrites=true&w=majority`;
 
