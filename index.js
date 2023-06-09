8f99797e3c53664d20f91f6bb06396d385846225
MostofaAsik <mmostofaasik@gmail.com>
2023-06-09 21:46:54 +0600

    create jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
@@ -29,6 +30,16 @@ async function run() {
 
 
         const usersCollection = client.db('sportsDb').collection('users')
+
+
+
+        //JWT
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
 
         //<<<<< users related api >>>>>>>>>
         //user get api
