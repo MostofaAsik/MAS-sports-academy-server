8de3cfdbb27a96a51d68329ad5c4ce2832ac120f
MostofaAsik <mmostofaasik@gmail.com>
2023-06-10 07:14:06 +0600

    check instrutor

--- index.js
+++ index.js
@@ -89,6 +89,18 @@ async function run() {
         })
 
 
+        //check instructor
+        app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                res.send({ instructor: false })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { instructor: user?.role === 'instructor' }
+            res.send(result)
+        })
+
         //make admin
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
