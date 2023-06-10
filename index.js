eb1c74c67e0f7fba8ce35483f067cc56de7c7113
MostofaAsik <mmostofaasik@gmail.com>
2023-06-10 06:49:16 +0600

    check admin

--- index.js
+++ index.js
@@ -76,6 +76,19 @@ async function run() {
             res.send(result)
         })
 
+        //check admin
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { admin: user?.role === 'admin' }
+            res.send(result)
+        })
+
+
         //make admin
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
