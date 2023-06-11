e555f037b4b4848464c6805712627723e7b49597
MostofaAsik <mmostofaasik@gmail.com>
2023-06-11 15:48:28 +0600

    approved

--- index.js
+++ index.js
@@ -157,6 +157,20 @@ async function run() {
             res.send(result)
         })
 
+        //approved
+        app.patch("/classes/approved/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: "approved",
+                },
+            };
+            const result = await classCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
